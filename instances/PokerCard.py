73b43d7e301015d1642583b420eb65c67c56295a
Damian Li <ke_2021@126.com>
2023-04-09 02:11:16 -0600

    update

--- instances/PokerCard.py
+++ instances/PokerCard.py
@@ -1,13 +1,6 @@
 import random
 from random import shuffle
-
-class PokerCard:
-    def __init__(self, suit: str, value: int):
-        self.suit = suit
-        self.value = value
-
-    def __repr__(self):
-        return f"{self.value} of {self.suit}"
+from .PokerCard import PokerCard
     
 class Stack:
     def __init__(self):
