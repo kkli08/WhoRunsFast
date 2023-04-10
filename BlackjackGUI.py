0580708765891889b44864192cba9ca1a37ad656
KN <kexun@ualberta.ca>
2023-04-09 18:06:05 -0600

    Add background image and music

--- BlackjackGUI.py
+++ BlackjackGUI.py
@@ -1,60 +1,50 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QPushButton
+from PyQt5.QtMultimedia import QSound, QMediaPlayer, QMediaContent
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QUrl
 from instances.Stack import Stack
 
-class BlackJackWindow(QWidget):
+class BlackJackWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-        # Set the window size and title
         self.setWindowTitle('Blackjack')
-        self.setGeometry(100, 100, 800, 600)
-        
-        # Create a new game
-        self.stack = Stack()
-        
-        self.user = []
-        self.bot = []
+        self.setFixedSize(1920, 1080)
 
-        self.user_score = 0
-        self.bot_score = 0
+        #set ./bg_images/bg.jpg as background
+        self.bg = QLabel(self)
+        self.bg.setPixmap(QPixmap('./bg_images/bg.jpg'))
+        self.bg.setGeometry(0, 0, 1920, 1080)
+        self.bg.setScaledContents(True)
 
-        self.playing = True
-        self.bust = False
+        # Create a new card game
+        self.game = Stack()
 
-        # Start the game
-        self.start()
-        
-        # Show the window
-        self.show()
+        start_button = QPushButton('Start')
+        start_button.clicked.connect(self.start_game)
+        #keep the button on the top and fixed, center it
+        start_button.setFixedSize(200, 100)
+        start_button.move(800, 500)
+        start_button.setParent(self)
 
-    def start(self):
-        # Shuffle the deck
-        self.stack.shuffle()
-        
-        # Draw 1 card for the user and bot
-        self.user.append(self.stack.cards.pop())
-        self.user.append(self.stack.cards.pop())
-        self.user.append(self.stack.cards.pop())
-        self.user.append(self.stack.cards.pop())
-        
-        # Display the cards
-        self.display_cards()
+        #set the font size of the button
+        font = start_button.font()
+        font.setPointSize(30)
+        start_button.setFont(font)
 
-    def display_cards(self):
-        # Display the user's cards
-        for i, card in enumerate(self.user):
-            card_label = QLabel(self)
-            card_label.setPixmap(QPixmap('card_images/{}_of_{}.png'.format(card.value, card.suit)))
+        #set ./bg_images/huanledoudizhu.mp3 as background music, loop forever
+        self.bg_music = QMediaPlayer()
+        self.bg_music.setMedia(QMediaContent(QUrl.fromLocalFile('./bg_images/huanledoudizhu.mp3')))
+        self.bg_music.setVolume(50)
+        self.bg_music.play()
 
-            # Move the card to the correct position
-            card_label.move(100 + (i * 100), 100)
-            card_label.show()
+        # Set up the GUI
+        # self.initUI()
 
-
-
+    def start_game(self):
+        pass
 if __name__ == '__main__':
     app = QApplication([])
     window = BlackJackWindow()
+    window.show()
+
     app.exec_()
